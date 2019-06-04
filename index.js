df269e384f554e665be4da60184542bd3e2c81a4
Collin Prusse <collinprusse@mac.com>
2019-06-03 22:27:22 -0600

    webhooks incorporated

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const passport = require('passport');
 const keys = require('./config/keys');
 const bodyParser = require('body-parser');
 require('./models/User');
+require('./models/Survey');
 require('./services/passport');
 
 
@@ -24,6 +25,7 @@ app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
+require('./routes/surveyRoutes')(app);
 
 if (process.env.NODE_ENV === 'production'){
   //Express will serve up production assets like main.js file or main.css
